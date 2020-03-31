436f1c8b584fc7c270b6c651701b3f1a5f863fb3
geraldgsh <geraldgsh@hotmail.com>
2020-04-01 05:01:43 +0800

    added cards

--- src/modules/about.js
+++ src/modules/about.js
@@ -1,5 +1,24 @@
 const about = () => {
-  const info = document.createElement('div');
+  const info = document.getElementById('content');
+  const info = ``;
   return info;
 };
 export default about;
+
+{/* <div class="canvas rows columns is-gapless">
+  <div class="container is-fluid">
+    <div class="column is-half">
+      <div class="row">
+        <div class="intro"> 
+          <h3 class='title is-4 has-text-black info'>
+            Hello. My name is <span class="highlight">Gerald Goh.</span>
+            I enjoy blending aesthetics with sophistication. Besides
+            coding, I am keen on science and engineering that I've pursued
+            them academically. On my travel, I'll be trigger ready on my 
+            camera or looking for a photogenic location to launch my drone.                               
+          </h3> 
+        </div>
+      </div>
+    </div>
+  </div>
+</div> */}
