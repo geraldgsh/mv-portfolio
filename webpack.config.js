0dd3892ef78f5b5b78cd21af02e8f5e403885713
geraldgsh <geraldgsh@hotmail.com>
2020-04-12 17:56:14 +0800

    transpile

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
