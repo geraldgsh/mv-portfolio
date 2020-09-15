e94d6fc402babdcb34e68b1500d957ca8191bbbc
geraldgsh <geraldgsh@hotmail.com>
2020-09-15 15:45:01 +0800

    ga update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,27 +1,23 @@
 /* eslint-disable react/jsx-props-no-spreading, react/react-in-jsx-scope */
 /* eslint-disable react/prop-types */
+import { Fragment } from 'react'
 import '../styles/style.scss';
 import Head from 'next/head';
+import * as gtag from 'common/src/lib/gtag'
+
+// Notice how we track pageview when route is changed
+Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
+    <Fragment>
       <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-178007015-1"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){
-            dataLayer.push(arguments)
-          }
-          gtag('js', new Date());
-          gtag('config', 'UA-178007015-1');
-        </script>
         <title>Gerald Goh</title>
       </Head>
       <div>
         <Component {...pageProps} />
       </div>
-    </div>
+    </Fragment>
   );
 }
 
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,17 +1,48 @@
+import { Fragment } from 'react'
 import Document, {
   Html, Head, Main, NextScript,
 } from 'next/document';
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
     const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const isProduction = process.env.NODE_ENV === 'production'
+    return {
+      ...initialProps,
+      isProduction,
+    };
   }
 
   render() {
+    const { isProduction } = this.props
     return (
       <Html>
         <Head>
+        {isProduction && (
+            <Fragment>
+              {/* Global Site Tag (gtag.js) - Google Analytics */}
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', '${GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+                }}
+              />
+            </Fragment>
+          )}
           <link rel="shortcut icon" href="/static/favicon.ico" />
         </Head>
         <body>
